f8ebb11d48fd09c5ed25a53304a59b191bbef527
Stephen Breyer-Menke <steve.bm@gmail.com>
2013-11-05 11:36:45 +0200

    Add sprite to assets precompile

--- lib/gemojifyjs/engine.rb
+++ lib/gemojifyjs/engine.rb
@@ -1,4 +1,7 @@
 module Gemojifyjs
   class Engine < Rails::Engine
+      initializer "gemojifyjs.assets.precompile" do |app|
+        app.config.assets.precompile += %w(emoji-sprite.png)
+      end
   end
 end
